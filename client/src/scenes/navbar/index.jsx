ba98ac0ec0588778a8c3502fe7eedc9f46e127eb
Kevin Baker <70351842+KevinRandallBaker@users.noreply.github.com>
2023-02-11 12:00:38 -0500

    Adding flex object and use previously defined theme settings.

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,53 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  LightMode,
+  Notifactions,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "react-router-dom";
+import { useNavigate } from "react-redux";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isMobileMenuToggled, setIsMobilMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 100px)");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.background.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName}${user.lastName}`;
+
+  return (
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography fontWeighit="bold" fontSize="clamp(1rem,2rem,2.25rem)">
+          Sociopedia
+        </Typography>
+      </FlexBetween>
+    </FlexBetween>
+  );
 };
 export default Navbar;
